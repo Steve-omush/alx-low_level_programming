2a49050102e7850dbedfc96814e58eb6a92a60e5
Steve-omush <steveprogram60@gmail.com>
2023-09-08 14:16:25 -0700

    File 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	 /* Get the character to be written */
+	 char alphabet = 'a';
+
+	 /* Write the Character to stdout */
+	 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		 putchar(alphabet);
+
+	 putchar('\n');
+
+	 return (0);
+}
