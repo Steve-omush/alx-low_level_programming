68c83039c0cebbb57e51767748a4aece3fc37cb4
Steve-omush <steveprogram60@gmail.com>
2023-09-14 10:37:44 -0700

    File 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: number of times to be printed
+ * Return: Nothing
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < n)
+		{
+			_putchar('_');
+
+			i++;
+		}
+		_putchar('\n');
+	}
+}
