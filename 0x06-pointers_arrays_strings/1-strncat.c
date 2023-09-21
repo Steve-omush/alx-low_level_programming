cbcce91a9e8276ba74a3b53d00fba1c1beee13f0
Steve-omush <steveprogram60@gmail.com>
2023-09-21 01:17:56 -0700

    File 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,8 +6,7 @@
 /**
  * _strncat - concatenates two strings
  * @dest: char pointer to a string
- * @src: char pointer to a string to be concatenated
- *
+ * @src: char pointer that concatenates
  * Return: pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
@@ -15,13 +14,13 @@ char *_strncat(char *dest, char *src, int n)
 	char *s1 = dest;
 
 	if ((dest == NULL) && (src == NULL))
+	{
 		return NULL;
-
+	}
 	while (*s1 != '\0')
 	{
 		s1++;
 	}
-
 	while (n--)
 	{
 		if (!(*s1++ = *src++))
@@ -29,7 +28,6 @@ char *_strncat(char *dest, char *src, int n)
 			return (dest);
 		}
 	}
-
 	*s1 = '\0';
 
 	return (dest);
