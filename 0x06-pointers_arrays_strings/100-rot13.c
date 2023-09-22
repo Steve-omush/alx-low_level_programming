97c1b061820ce5507756b2497eaa6a6237f092c6
Steve-omush <steveprogram60@gmail.com>
2023-09-22 08:56:18 -0700

    File 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: pointer input string
+ *
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	char *encoded = s;
+
+	while (*s)
+	{
+		if ((*s >= 'a' && *s <= 'm') || (*s >= 'A' && *s <= 'M'))
+		{
+			*s += 13;
+		}
+		else if ((*s >= 'n' && *s <= 'z') || (*s >= 'N' && *s <= 'Z'))
+		{
+			*s -= 13;
+		}
+		s++;
+	}
+	return (encoded);
+}
