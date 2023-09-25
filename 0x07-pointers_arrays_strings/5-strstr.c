713dc472667147920fcb210b022c6e956d5f6d43
Steve-omush <steveprogram60@gmail.com>
2023-09-25 15:37:53 -0700

    File 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,11 +3,11 @@
 #include <string.h>
 
 /**
- * _strstr - locates a strinG
- * @haystack: string pointer
- * @needle: character to be located
+ * _strstr - locates a substring
+ * @haystack: input string
+ * @needle: input string to haystack
  *
- * Return: pointer to the beginning
+ * Return: pointer to substring, or NULL if not found
  */
 char *_strstr(char *haystack, char *needle)
 {
@@ -21,11 +21,15 @@ char *_strstr(char *haystack, char *needle)
 		while (needle[b + c] != '\0' && haystack[a + c] != '\0' && needle[b + c] == haystack[a + c])
 		{
 			if (haystack[a + c] != needle[b + c])
+			{
 				break;
+			}
 			c++;
 		}
 		if (needle[b + c] == '\0')
+		{
 			return (&haystack[a]);
+		}
 		b++;
 		a++;
 	}
