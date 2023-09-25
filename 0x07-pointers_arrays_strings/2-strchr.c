a4678e34c533114a41ca6b24a3a35e8a7cbb1235
Steve-omush <steveprogram60@gmail.com>
2023-09-25 09:06:40 -0700

    File 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,6 +5,7 @@
  * _strchr - locates a character in string
  * @s: input string to be searched
  * @c: charcter to search for
+ * Return: returns pointer to c position
  */
 char *_strchr(char *s, char c)
 {
