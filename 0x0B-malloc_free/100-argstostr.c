76311d7f321e0295c18bcb57686dc4ff6f8e8859
Steve-omush <steveprogram60@gmail.com>
2023-10-04 02:57:41 -0700

    File 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -26,7 +26,7 @@ char *argstostr(int ac, char **av)
 			total_length += arg_length + 1;
 		}
 	}
-	result = (char *)malloc(total_length * sizeof(char));
+	result = (char *)malloc((total_length + 1) * sizeof(char));
 	if (result == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
