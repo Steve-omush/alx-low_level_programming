3b1cdfe7dbc581490020ee1b99fa4e26b552cc48
Steve-omush <steveprogram60@gmail.com>
2023-10-11 03:21:25 -0700

    File 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of main
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int num_bytes, start_index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (start_index = 0; start_index < num_bytes; start_index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (start_index == num_bytes - 1)
+			continue;
+		printf(" ");
+		address ++;
+	}
+	printf("\n");
+	return (0);
+}
