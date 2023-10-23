1117035ce8e67e1de2ce1858a967947c03cb99d2
Steve-omush <steveprogram60@gmail.com>
2023-10-23 09:06:33 -0700

    File correction on 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_listint - prints elements listint_t list.
+ * print_listint - A function that prints all the elements in the list.
  * @h: pointer to head.
  *
- * Return: number of nodes.
+ * Return: Number of nodes.
  */
 size_t print_listint(const listint_t *h)
 {
